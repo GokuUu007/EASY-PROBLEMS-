76b335996f60cbae5c41d21a2b33f46341e282a1
Akshat madhavan <112942320+GokuUu007@users.noreply.github.com>
2022-11-22 16:44:07 +0530

    Update findLeader.cpp

--- findLeader.cpp
+++ findLeader.cpp
@@ -2,6 +2,9 @@
 #include <vector>
 using namespace std;
 
+// Given an array A of positive integers. Your task is to find the leaders in the array. 
+// An element of array is leader if it is greater than or equal to all the elements to its right side. The rightmost element is always a leader. 
+
 void leaders(int a[], int n)
 {
     // Code here
